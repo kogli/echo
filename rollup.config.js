63121ec8722b8c0464000858112b4178c379aa7e
Jan Neumann <janneumannprg@gmail.com>
2018-03-03 18:08:50 +0100

    Added TypeScript declarations generation for TypeScript integration support. Updated Rollup and replaced rollup-plugin-typescript with rollup-plugin-typescript2.

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
-import typescript from 'rollup-plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import babel from 'rollup-plugin-babel';
 
 export default {
   entry: './src/echo.ts',
   dest: './dist/echo.js',
+  format: 'cjs',
   plugins: [
     typescript(),
     babel({
